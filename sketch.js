a8222d123c8b3ca3fb8254957f7197f9855581b8
KarambitXT <88159769+KarambitXT@users.noreply.github.com>
2021-07-29 13:53:54 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var box
+function setup() {
+  createCanvas(400,400);
+box = createSprite(200,200,20,20)
+box.shapeColor = "white"
+}
+
+function draw() 
+{
+  background(30);
+if (keyIsDown(RIGHT_ARROW)) {
+  box.x = box.x + 1
+}
+if (keyIsDown(LEFT_ARROW)) {
+  box.x = box.x - 1
+}
+if (keyIsDown(DOWN_ARROW)) {
+  box.y = box.y + 1
+}
+if (keyIsDown(UP_ARROW)) {
+  box.y = box.y - 1
+}
+  drawSprites()
+}
+
+
+
+
